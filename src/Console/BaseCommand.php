e0fb8900c5ce7189c087261caf5e2003fd549eb3
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-29 11:08:29 +0700

    feat: init command

---
+++ src/Console/BaseCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ahc\Phint\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+class BaseCommand extends Command
+{
+    /** @var InputInterface */
+    protected $input;
+
+    /** @var InputInterface */
+    protected $output;
+
+    protected function prompt($prompt, $default = null, callable $validator = null)
+    {
+        $helper   = $this->getHelper('question');
+        $question = new Question($prompt, $default);
+
+        $question->setValidator($validator ?: function ($value) {
+            if (empty($value)) {
+                throw new \InvalidArgumentException('Please provide non empty value');
+            }
+
+            return $value;
+        });
+
+        return $this->getHelper('question')->ask($this->input, $this->output, $question);
+    }
+}
---
+++ src/Console/InitCommand.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Ahc\Phint\Console;
+
+use Ahc\Phint\Util\Git;
+use Ahc\Phint\Util\Path;
+use Ahc\Phint\Util\Inflector;
+use Ahc\Phint\Generator\TwigGenerator;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends BaseCommand
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('init')
+            ->setDescription('Create a bare new PHP project')
+            ->addArgument('project', InputArgument::REQUIRED, 'The project name')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force initialization even if the project exists')
+            ->addOption('description', 'd', InputArgument::OPTIONAL, 'Project description')
+            ->addOption('name', 'm', InputArgument::OPTIONAL, 'Vendor full name, defaults to git name')
+            ->addOption('username', 'u', InputArgument::OPTIONAL, 'Vendor handle or username',
+                getenv('GITHUB_USERNAME')
+            )
+            ->addOption('email', 'e', InputArgument::OPTIONAL, 'Vendor email, defaults to git email')
+            ->addOption('namespace', 's', InputArgument::OPTIONAL, 'Root namespace')
+            ->addOption('year', 'y', InputArgument::OPTIONAL, 'License Year, defaults to date("Y")',
+                date('Y'))
+            ->addOption('type', 't', InputArgument::OPTIONAL, 'Project type, defaults to library',
+                'library')
+        ;
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input  = $input;
+        $this->output = $output;
+
+        $output->writeln('<info>Preparing ...</info>');
+
+        $projectPath = $this->prepareProjectPath();
+        $parameters  = $this->collectParameters();
+
+        $output->writeln('<comment>Generating files ...</comment>');
+
+        $this->generate($projectPath, $parameters);
+
+        $output->writeln('<comment>Done</comment>');
+    }
+
+    protected function prepareProjectPath()
+    {
+        $path = $this->input->getArgument('project');
+
+        if (!(new Path)->isAbsolute($path)) {
+            $path = getcwd() . '/' . $path;
+        }
+
+        if (file_exists($path)) {
+            if (!$this->input->getOption('force')) {
+                throw new \InvalidArgumentException('Something with the same name already exists!');
+            }
+
+            $this->output->writeln('<error>You have set force flag, existing files will be overwritten</error>');
+        } else {
+            mkdir($path, 0777, true);
+        }
+
+        return $path;
+    }
+
+    protected function collectParameters()
+    {
+        $git         = new Git;
+        $inflector   = new Inflector;
+        $project     = $this->input->getArgument('project');
+        $Project     = $inflector->words($project);
+        $year        = $this->input->getOption('year');
+        $type        = $this->input->getOption('type');
+        $vendorName  = $this->input->getOption('name') ?: $git->getConfig('user.name');
+        $vendorEmail = $this->input->getOption('email') ?: $git->getConfig('user.email');
+
+        $description = $this->input->getOption('description') ?: $this->prompt(
+            'Project description [<comment>A brief intro about this project</comment>]: '
+        );
+
+        $username = $this->input->getOption('username') ?: $this->prompt(
+            'Vendor Handle [<comment>Often your github username, set GITHUB_USERNAME env variable to automate</comment>]: '
+        );
+
+        $namespace = $inflector->stuldyCase($username) . '/' . $inflector->stuldyCase($project);
+        $namespace = $this->input->getOption('namespace') ?: $this->prompt(
+            'Root Namespace [<comment>Defaults to ' . $namespace . '</comment>]: ',
+            $namespace
+        );
+
+        $namespace = str_replace('/', '\\\\', $namespace);
+        $keywords  = ['php', $project];
+
+        return compact(
+            'year', 'project', 'vendorName', 'vendorEmail', 'description',
+            'username', 'namespace', 'keywords', 'Project', 'type'
+        );
+    }
+
+    protected function generate($projectPath, array $parameters)
+    {
+        $templatePath = __DIR__ . '/../../resources';
+        $cachePath    = __DIR__ . '/../../.cache';
+
+        $generator = new TwigGenerator($templatePath, $cachePath);
+
+        $generator->generate($projectPath, $parameters);
+    }
+}
