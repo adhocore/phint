13ef26b81ad7b67f8034c5c8ad52fc2ad17d41e2
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-29 17:06:11 +0700

    feat: composer util

---
+++ src/Util/Composer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ahc\Phint\Util;
+
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class Composer extends Executable
+{
+    public function __construct($workDir = null, $binary = null)
+    {
+        parent::__construct($workDir, $binary ?: 'composer');
+    }
+
+    public function createProject($project, $using)
+    {
+        $this->runCommand(sprintf('create-project %s %s', $using, $project));
+
+        return $this;
+    }
+
+    public function install($project)
+    {
+        $this->runCommand(sprintf('create-project %s %s', $using, $project));
+
+        return $this;
+    }
+}
