2e305d7857bfe9cf978a638a5621ac9fcc846375
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-29 17:05:01 +0700

    feat: common executable util

---
+++ src/Util/Executable.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ahc\Phint\Util;
+
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Output\OutputInterface;
+
+abstract class Executable
+{
+    /** @var OutputInterface */
+    protected $output;
+
+    /** @var string The binary executable */
+    protected $binary;
+
+    /** @var string */
+    protected $workDir;
+
+    public function __construct($workDir = null, $binary = null)
+    {
+        $this->workDir = $workDir;
+        $this->binary  = $binary ? '"' . $binary . '"' : $binary;
+    }
+
+    public function withOutput(OutputInterface $output = null)
+    {
+        $this->output = $output;
+
+        return $this;
+    }
+
+    protected function findBinary($binary)
+    {
+        if (\is_executable($binary)) {
+            return $binary;
+        }
+
+        $finder = new ExecutableFinder();
+
+        return $finder->find($binary) ?: $binary;
+    }
+
+    /**
+     * Runs the command using underlying binary.
+     *
+     * @param string $command
+     *
+     * @return string|null The output of command.
+     */
+    protected function runCommand($command)
+    {
+        $self = $this;
+        $proc = new Process($this->binary . ' ' . $command, $this->workDir, null, null, null);
+
+        $proc->run(!$this->output ? null : function ($type, $buffer) use ($self) {
+            $self->output->write($buffer);
+        });
+
+        if ($proc->isSuccessful()) {
+            return $proc->getOutput();
+        }
+    }
+}
