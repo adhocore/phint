8491990927503a6e25f3b22c2ebb21fe96e73cf5
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-29 17:37:05 +0700

    feat: array util

---
+++ src/Util/Arr.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ahc\Phint\Util;
+
+class Arr
+{
+    /**
+     * @see http://php.net/array_merge_recursive#92195
+     *
+     * @param array $array1
+     * @param array $array2
+     *
+     * @return array
+     */
+    public function mergeRecursive(array $array1, array $array2)
+    {
+        $merged = $array1;
+
+        foreach ($array2 as $key => &$value) {
+            if (\is_array($value) && isset($merged[$key]) && \is_array($merged[$key])) {
+                $merged[$key] = Arr::mergeRecursive($merged[$key], $value);
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
+    }
+
+}
