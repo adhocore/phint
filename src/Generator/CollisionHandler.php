548cd65c9dbac8c52b39c83b90e7f2c4f62a5c9a
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-29 17:07:02 +0700

    feat: collosion handler

---
+++ src/Generator/CollisionHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ahc\Phint\Generator;
+
+use Ahc\Phint\Util\Path;
+
+class CollisionHandler implements CollisionHandlerInterface
+{
+    public function __construct(Path $pathUtil = null)
+    {
+        $this->pathUtil = $pathUtil ?: new Path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($targetFile, $newContent, array $parameters = null)
+    {
+        switch ($this->pathUtil->getExtension($targetFile)) {
+            case 'json':
+                $this->mergeJson($targetFile, $newContent);
+
+                break;
+        }
+    }
+
+    protected function mergeJson($targetFile, $newContent)
+    {
+        $oldJson = $this->pathUtil->readAsJson($targetFile);
+        $newJson = \json_decode($newContent, true);
+        $merged  = \array_merge($oldJson, $newJson);
+
+        $this->pathUtil->writeFile(
+            $targetFile,
+            \json_encode($merged, \JSON_PRETTY_PRINT)
+        );
+    }
+}
