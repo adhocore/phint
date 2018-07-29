c69f0adeef531b45502892f6e0273a164dae1382
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-29 17:17:14 +0700

    feat(generator): test stubs generator

--- src/Generator/TwigGenerator.php
+++ src/Generator/TwigGenerator.php
@@ -3,6 +3,7 @@
 namespace Ahc\Phint\Generator;
 
 use Ahc\Phint\Util\Path;
+use Ahc\Phint\Util\Inflector;
 use Symfony\Component\Finder\Finder;
 
 class TwigGenerator implements GeneratorInterface
@@ -12,6 +13,9 @@ class TwigGenerator implements GeneratorInterface
 
     /** @var Path */
     protected $pathUtil;
+
+    /** @var Inflector */
+    protected $inflector;
 
     /** @var string|array */
     protected $templatePath;
@@ -25,11 +29,17 @@ class TwigGenerator implements GeneratorInterface
         'package.json' => true,
     ];
 
+    /** @var array Templates only loaded by some specific commands */
+    protected $commandTemplates = [
+        'test' => true,
+    ];
+
     public function __construct(string $templatePath, string $cachePath)
     {
         $this->templatePath = $templatePath;
         $this->cachePath    = $cachePath;
         $this->pathUtil     = new Path;
+        $this->inflector    = new Inflector;
     }
 
     /**
@@ -49,6 +59,30 @@ class TwigGenerator implements GeneratorInterface
         }
     }
 
+    public function generateTests(array $testMetadata, array $parameters): int
+    {
+        if (!$this->twig) {
+            $this->initTwig();
+        }
+
+        $generated = 0;
+
+        foreach ($testMetadata as $metadata) {
+            // Skip existing
+            if (\is_file($targetFile = $metadata['testPath'])) {
+                continue;
+            }
+
+            $generated++;
+
+            $content = $this->twig->render('tests/test.twig', $metadata + $parameters);
+
+            $this->pathUtil->writeFile($targetFile, $content);
+        }
+
+        return $generated;
+    }
+
     protected function initTwig()
     {
         $options = [
@@ -65,6 +99,10 @@ class TwigGenerator implements GeneratorInterface
             new \Twig_Loader_Filesystem($this->templatePath),
             $options
         );
+
+        $this->twig->addFilter(new \Twig_SimpleFilter('snake', function ($x) {
+            return $this->inflector->snakeCase($x);
+        }));
     }
 
     protected function findTemplates(string $templatePath)
@@ -111,6 +149,10 @@ class TwigGenerator implements GeneratorInterface
 
         if (isset($this->projectTemplates[$name])) {
             return $parameters['type'] === 'project';
+        }
+
+        if (isset($this->commandTemplates[$name])) {
+            return false;
         }
 
         return true;
