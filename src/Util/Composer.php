d576d3f4b3b96eb1a777903da75f6e75cc017209
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-29 17:17:47 +0700

    feat(composer): add config getter and autoload dumper

--- src/Util/Composer.php
+++ src/Util/Composer.php
@@ -4,6 +4,9 @@ namespace Ahc\Phint\Util;
 
 class Composer extends Executable
 {
+    /** @var array Content of composer.json decoded */
+    protected $config = [];
+
     /** @var string The binary executable */
     protected $binary = 'composer';
 
@@ -26,5 +29,30 @@ class Composer extends Executable
         $this->runCommand('update --prefer-dist --optimize-autoloader --no-suggest');
 
         return $this;
+    }
+
+    public function dumpAutoload()
+    {
+        $this->runCommand('dump-autoload --optimize');
+
+        return $this;
+    }
+
+    public function config(string $key, $default = null)
+    {
+        if (!$this->config) {
+            $this->config = (new Path)->readAsJson($this->workDir . '/composer.json');
+        }
+
+        $temp = $this->config;
+        foreach (\explode('.', $key) as $part) {
+            if (\is_array($temp) && \array_key_exists($part, $temp)) {
+                $temp = $temp[$part];
+            } else {
+                return $default;
+            }
+        }
+
+        return $temp;
     }
 }
