5b6d0b162a77c6caccc1fd9e63752115a0662e77
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-05 16:27:49 +0700

    Apply fixes from StyleCI (#27)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the PHINT package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https//:github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
 require_once __DIR__ . '/../vendor/autoload.php';
