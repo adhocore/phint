5f8dabebe2ade11b5a1e7da8286954e4c13bacac
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-25 08:39:53 +0700

    feat(util.meta): add method metadata util

--- src/Util/Metadata.php
+++ src/Util/Metadata.php
@@ -51,4 +51,43 @@ class Metadata
 
         return $metadata;
     }
+
+    public function forMethod(string $classFqcn, string $method, bool $docblock = false): array
+    {
+        $reflMethod = (new \ReflectionClass($classFqcn))->getMethod($method);
+
+        return $this->forReflectionMethod($reflMethod);
+    }
+
+    public function forReflectionMethod(\ReflectionMethod $method, bool $docblock = false): array
+    {
+        $params = [];
+        $parser = new DocBlock($method);
+
+        foreach ($parser->find('param') as $param) {
+            $params[] = \preg_replace(['/(.*\$\w+)(.*)/', '/ +/'], ['$1', ' '], $param->getValue());
+        }
+
+        if (null !== $return = $parser->first('return')) {
+            $return = \preg_replace('/(\S+)(.*)/', '$1', $return->getValue());
+        }
+
+        $metadata = [
+            'isStatic'   => $method->isStatic(),
+            'isFinal'    => $method->isFinal(),
+            'isPublic'   => $method->isPublic(),
+            'isAbstract' => $method->isAbstract(),
+            'params'     => $params,
+            'return'     => $return,
+        ];
+
+        if ($docblock) {
+            $texts = $parser->texts();
+            $title = \array_shift($texts);
+
+            $metadata += \compact('title', 'texts');
+        }
+
+        return $metadata;
+    }
 }
